5cf5302c5e1a41a88c811e5540a327098360ea4c
Samuel Acosta Aristizábal <88010033+itssam0@users.noreply.github.com>
2022-02-23 16:05:34 -0500

    Código de lectura de cvs mapa

---
+++ codigo/pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+archivo=pd.read_csv("https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/calles_de_medellin_con_acoso.csv", sep=";")
+ 
+print(archivo)
