e33a8e6f5894366f69a79d225a721e6037b6aff3
Samuel Acosta Aristizábal <88010033+itssam0@users.noreply.github.com>
2022-04-17 17:04:47 -0500

    Delete pandas.py

--- codigo/pandas.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-archivo=pd.read_csv("https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/calles_de_medellin_con_acoso.csv", sep=";")
- 
-print(archivo)
